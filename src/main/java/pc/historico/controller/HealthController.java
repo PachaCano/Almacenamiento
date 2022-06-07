0e3903e110b39f84457ae8d75117b8374dfad054
Leonel <rpacha731@alumnos.iua.edu.ar>
2022-06-07 01:18:06 -0300

    ...

--- src/main/java/pc/historico/controller/HealthController.java
+++ src/main/java/pc/historico/controller/HealthController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/health")
+@RequestMapping("/api/historico")
 public class HealthController {
 
-    @GetMapping("")
+    @GetMapping("/health")
     public ResponseEntity<String> healthCheck() {
         return new ResponseEntity<>("OK", HttpStatus.OK);
     }
